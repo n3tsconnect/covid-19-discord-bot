f66b151d7be08ce2afa9a37999be7ec327284dbb
adrianardizza <adrian.ardizza@gmail.com>
2020-03-20 20:59:42 +0700

    Added new links to information.js

--- commands/information.js
+++ commands/information.js
@@ -15,8 +15,12 @@ module.exports = {
             description: 'Here are some recommended and up to date resources on the situation regarding the 2019-2020 COVID-19 Outbreak:'
             + '\n\n **General Information:**'
             + '\n [A Compact and Concise PSA on COVID-19 (source: u/ilikelegoandcrackers from r/Canada)](https://www.reddit.com/r/canada/comments/fghd23/psa_regarding_covid19_a_warning/)'
-            + '\n\n [WHO Informational Page on The Outbreak](https://www.who.int/emergencies/diseases/novel-coronavirus-2019)'
+            + '\n\n [WHO Informational Page on COVID-19](https://www.who.int/emergencies/diseases/novel-coronavirus-2019)'
+            + '\n\n [CDC Informational Page on COVID-19](https://www.cdc.gov/coronavirus/2019-ncov/index.html)'
             + '\n\n [Regional Information (Indonesia) - KAWALCOVID19](https://kawalcovid19.id/)'
+            + '\n\n [Regional Information (Indonesia) - Govt. Information Site on COVID-19](https://www.covid19.go.id/)'
+            + '\n\n [Regional Information (Indonesia) - Jakarta Govt. COVID-19 Site](https://corona.jakarta.go.id/id)'
+            + '\n\n [Regional Information (Indonesia) - Jabar Govt. COVID-19 Site](https://pikobar.jabarprov.go.id/)'
 
             + '\n\n **Interactive Maps:**'
             + '\n [John Hopkins University Global Interactive Map](https://coronavirus.jhu.edu/map.html)'
