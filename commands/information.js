f7b74057383560d581372f0f0877db4f95bbf680
adrianardizza <adrian.ardizza@gmail.com>
2020-03-21 12:11:46 +0700

    Moved some links to new Regional Info section.

--- commands/information.js
+++ commands/information.js
@@ -1,6 +1,7 @@
-const { general, maps } = require('./info/info.json')
+const { general, maps, regional } = require('./info/info.json')
 var generalInfo = ""
     mapsInfo = ""
+    regionalInfo = ""
     
 
 module.exports = {
@@ -16,6 +17,10 @@ module.exports = {
             mapsInfo += `\n[${info['description']}](${info['link']})\n`
         })
 
+        regional.forEach((info) => {
+            regionalInfo += `\n[${info['description']}](${info['link']})\n`
+        })
+
         const infoEmbed = {
             color: 0x0099ff,
             title: 'Recommended Resources',
@@ -24,19 +29,23 @@ module.exports = {
             },
             description: 
             'Here are some recommended and up to date resources on the situation regarding the 2019-2020 COVID-19 Outbreak:'
-            + '\n\n **General Information:**'
+            + '\n\n **General Information:**' // General Information for COVID-19
             + generalInfo
-            + '\n **Interactive Maps:**'
+
+            + '\n **Interactive Maps:**' // Interactive Maps for COVID-19 (intl.)
             + mapsInfo
                 
-            + '\n**More information might be added to this command later.**'
-            ,
+            + '\n**Regional Information - Indonesia**' // Regional Information for COVID-19 (Indonesia)
+            + regionalInfo,
             thumbnail: {
                 url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTnz3li3an_k7fTdPCmol2xW8HrPiVFkzqEEbtyN6NvnBvnQo0G',
             },
             fields: [
             ],
             timestamp: new Date(),
+            footer: {
+                text: 'More information might be added to this command later.'
+            }
 
         };
 
